0224a60d99e187fd3f920c75264de1cd544112d3
kevin5806 <kevinleoni58@gmail.com>
2023-11-02 21:53:22 +0100

    Update on.js

--- api/on.js
+++ api/on.js
@@ -1,32 +1,45 @@
-module.exports = async (req, res) => {
-    try {
-        const data = {
-            ResetType: 'On',
-            auth: {
-                username: process.env.USER,
-                password: process.env.PASS,
-            },
-        };
-
-        const apiUrl = `https://${process.env.IP}/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset`;
-
-        const https = require('https');
-        const fetch = await import('node-fetch');
-
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-            agent: new https.Agent({ rejectUnauthorized: false }),
-        };
-
-        const response = await fetch.default(apiUrl, options);
-
-        res.status(204).end();
-    } catch (error) {
-        console.error('Errore durante la richiesta POST:', error);
-        res.status(500).json({ error: 'Si è verificato un errore durante la richiesta POST' });
+// Importa il modulo node-fetch in modo dinamico
+import('node-fetch').then(async (fetch) => {
+    // Funzione principale per accendere il server
+    async function accendiServer() {
+      // Recupera le variabili d'ambiente
+      const idracIP = process.env.IP;
+      const idracUser = process.env.USER;
+      const idracPassword = process.env.PASS;
+  
+      // Crea l'URL per la richiesta Redfish
+      const redfishUrl = `https://${idracIP}/redfish/v1/Systems/1/Actions/ComputerSystem.Reset`;
+  
+      // Opzioni per la richiesta Redfish
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Authorization': `Basic ${Buffer.from(`${idracUser}:${idracPassword}`).toString('base64')}`,
+        },
+      };
+  
+      try {
+        const response = await fetch(redfishUrl, requestOptions);
+  
+        if (response.status === 204) {
+          console.log('Richiesta Redfish riuscita. Il server è stato acceso.');
+        } else {
+          console.error('Errore durante la richiesta Redfish:', response.status);
+        }
+      } catch (error) {
+        console.error('Errore durante la richiesta Redfish:', error.message);
+      }
     }
-};
+  
+    // Gestisce la richiesta HTTP in entrata da Vercel
+    module.exports = async (req, res) => {
+      if (req.method === 'POST') {
+        // Esegui la funzione per accendere il server
+        await accendiServer();
+        res.status(200).send('Richiesta Redfish in corso.');
+      } else {
+        res.status(405).send('Metodo non consentito.');
+      }
+    };
+  });
+  
