eed4f66a2691074fd5911acd93ca4e4a7d42837e
kevin5806 <kevinleoni58@gmail.com>
2023-11-02 21:15:48 +0100

    ignore ssl

--- api/off.js
+++ api/off.js
@@ -1,30 +1,26 @@
 module.exports = async (req, res) => {
     try {
-
         const data = {
             ResetType: 'GracefulShutdown',
             auth: {
                 username: process.env.USER,
                 password: process.env.PASS,
-            }
-        }
-
+            },
+        };
 
         const apiUrl = `https://${process.env.IP}/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset`;
 
+        const https = await import('https');
         const fetch = await import('node-fetch');
 
         const options = {
-
             method: 'POST',
-
             headers: {
                 'Content-Type': 'application/json',
             },
-
-            body: JSON.stringify(data)
-
-        }
+            body: JSON.stringify(data),
+            agent: new https.Agent({ rejectUnauthorized: false }),
+        };
 
         const response = await fetch.default(apiUrl, options);
 
@@ -35,4 +31,4 @@ module.exports = async (req, res) => {
         res.status(500).json({ error: 'Si è verificato un errore durante la richiesta POST' });
 
     }
-}
+};
