177899f2c3dc0ae87d8d79e074aa22ca8e042c46
kevin5806 <kevinleoni58@gmail.com>
2023-11-02 19:00:08 +0100

    upload

---
+++ on.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+require('dotenv').config();
+
+const idracIpAddress = process.env.IP;
+const idracUsername = process.env.USER;
+const idracPassword = process.env.PASS;
+
+const powerControlEndpoint = `https://${idracIpAddress}/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset`;
+
+const powerControlData = {
+    ResetType: 'On',
+};
+
+const config = {
+    auth: {
+        username: idracUsername,
+        password: idracPassword,
+    },
+};
+
+axios.post(powerControlEndpoint, powerControlData, config);
