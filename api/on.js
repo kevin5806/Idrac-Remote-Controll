5f6737f42d014334d644cf915dcfdcd839dfeec0
kevin5806 <kevinleoni58@gmail.com>
2023-11-02 19:13:05 +0100

    edit

--- api/on.js
+++ api/on.js
@@ -1,22 +1,25 @@
-const axios = require('axios');
+export default async function handler(request, response) {
+    const axios = require('axios');
 
-require('dotenv').config();
+    require('dotenv').config();
 
-const idracIpAddress = process.env.IP;
-const idracUsername = process.env.USER;
-const idracPassword = process.env.PASS;
+    const idracIpAddress = process.env.IP;
+    const idracUsername = process.env.USER;
+    const idracPassword = process.env.PASS;
 
-const powerControlEndpoint = `https://${idracIpAddress}/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset`;
+    const powerControlEndpoint = `https://${idracIpAddress}/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset`;
 
-const powerControlData = {
-    ResetType: 'On',
-};
+    const powerControlData = {
+        ResetType: 'On',
+    };
 
-const config = {
-    auth: {
-        username: idracUsername,
-        password: idracPassword,
-    },
-};
+    const config = {
+        auth: {
+            username: idracUsername,
+            password: idracPassword,
+        },
+    };
 
-axios.post(powerControlEndpoint, powerControlData, config);
+    axios.post(powerControlEndpoint, powerControlData, config);
+    return response.status(200);
+}
