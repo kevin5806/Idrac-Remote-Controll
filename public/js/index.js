3776c4baff47fb488f0c08dad05f84a0ed8e52ed
kevin5806 <kevinleoni58@gmail.com>
2023-11-03 21:43:41 +0100

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,9 @@ const statusRes = "none";
 
 async function updateStatus() {
 
-    statusRes = await fetch('/api/status');
+    statusRes = await fetch('/api/status', { method: 'GET' });
+
+    console.log(statusRes);
 
     HTMLstatus.textContent = statusRes;
 
