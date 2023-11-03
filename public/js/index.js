b118f27f067e8e29f6bfb96492fe85dbd0bf675a
kevin5806 <kevinleoni58@gmail.com>
2023-11-03 23:01:16 +0100

    test

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,6 @@
 const HTMLstatus = document.getElementById("status");
+const HTMLinputPin = document.getElementById("inputPin");
+const HTMLpowerRes = document.getElementById("powerRes");
 
 let statusRes = "none";
 
@@ -14,4 +16,21 @@ async function updateStatus() {
         State: ${statusRes.State}
     `;
 
+}
+
+async function setPower(action) {
+    const body = {};
+
+    body.action = action;
+    body.pin = HTMLinputPin.value;
+
+    const res = await fetch('/api/power', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: body.json()
+    })
+
+    powerRes.innerHTML = `${res.json()}`;
 }
