3e5e35a5c9d0791b2f4f77efb7c40dc6b1a360bb
Kevin Leoni <kevinleoni58@gmail.com>
2023-11-03 11:45:37 +0100

    Update on.js

--- api/on.js
+++ api/on.js
@@ -1,45 +1,28 @@
-// Importa il modulo node-fetch in modo dinamico
-import('node-fetch').then(async (fetch) => {
-    // Funzione principale per accendere il server
-    async function accendiServer() {
-      // Recupera le variabili d'ambiente
-      const idracIP = process.env.IP;
-      const idracUser = process.env.USER;
-      const idracPassword = process.env.PASS;
-  
-      // Crea l'URL per la richiesta Redfish
-      const redfishUrl = `https://${idracIP}/redfish/v1/Systems/1/Actions/ComputerSystem.Reset`;
-  
-      // Opzioni per la richiesta Redfish
-      const requestOptions = {
-        method: 'POST',
-        headers: {
-          'Authorization': `Basic ${Buffer.from(`${idracUser}:${idracPassword}`).toString('base64')}`,
-        },
-      };
-  
-      try {
-        const response = await fetch(redfishUrl, requestOptions);
-  
-        if (response.status === 204) {
-          console.log('Richiesta Redfish riuscita. Il server è stato acceso.');
-        } else {
-          console.error('Errore durante la richiesta Redfish:', response.status);
-        }
-      } catch (error) {
-        console.error('Errore durante la richiesta Redfish:', error.message);
-      }
-    }
-  
-    // Gestisce la richiesta HTTP in entrata da Vercel
-    module.exports = async (req, res) => {
-      if (req.method === 'POST') {
-        // Esegui la funzione per accendere il server
-        await accendiServer();
-        res.status(200).send('Richiesta Redfish in corso.');
-      } else {
-        res.status(405).send('Metodo non consentito.');
-      }
+const axios = require('axios');
+
+exports.handler = async (event, context) => {
+  const { USER, PASS, IP } = process.env;
+  const redfishEndpoint = `https://${IP}/redfish/v1/Systems/1/Actions/ComputerSystem.Reset`;
+
+  const requestData = {
+    ResetType: "On",
+  };
+
+  const headers = {
+    Authorization: `Basic ${Buffer.from(`${USER}:${PASS}`).toString('base64')}`,
+  };
+
+  try {
+    const response = await axios.post(redfishEndpoint, requestData, { headers });
+    const data = response.data;
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
     };
-  });
-  
+  } catch (error) {
+    return {
+      statusCode: error.response.status,
+      body: JSON.stringify(error.response.data),
+    };
+  }
+};
